5fdd75d8807770d2031e49c04d7a59f4474a3b9a
4Moyede <krkdgud9@gmail.com>
2021-01-23 20:00:47 +0900

    [B_Add] Add search API

--- back/api/urls.py
+++ back/api/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from api import views
 
 urlpatterns = [
-    path('in/', views.SignIn.as_view()),
-    path('out/', views.SignOut.as_view()),
     path('search/', views.Search.as_view())
 ]
--- back/api/views.py
+++ back/api/views.py
@@ -7,16 +7,23 @@ from api.models import MolecularData
 from api.serializers import MolecularSerializer
 
 
-class SignIn(APIView):
-    def get(self, request, format=None):
-        return Response(status=status.HTTP_200_OK)
-
-class SignOut(APIView):
-    def get(self, request, format=None):
-        return Response(status=status.HTTP_200_OK)
-
 class Search(APIView):
     def get(self, request, format=None):
-        queryset = MolecularData.objects.all()
-        serializer = MolecularSerializer(queryset, many=True)
-        return Response({"molecular" : serializer.data}, status=status.HTTP_200_OK)
+        keywords = request.GET.get('keyword')
+        response = {
+            "OK" : [],
+            "NotFound" : [],
+            "Result" : []
+        }
+        
+        for keyword in keywords.split(','):
+            queryset = MolecularData.objects.filter(formula=keyword)
+            serializer = MolecularSerializer(queryset, many=True)
+            if serializer.data:
+                response['OK'].append(keyword)
+                response['Result'].append(serializer.data[0])
+            else:
+                response['NotFound'].append(keyword)
+            
+        return Response(response, status=status.HTTP_200_OK)
+
