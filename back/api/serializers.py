e8ca9be6d6520845fcc6db227a04d582493611ca
4Moyede <krkdgud9@gmail.com>
2021-02-12 17:23:46 +0900

    [Add] search Reagent property function

--- back/api/serializers.py
+++ back/api/serializers.py
@@ -1,6 +1,11 @@
 from rest_meets_djongo.serializers import DjongoModelSerializer
-from api.models import MaterialSafetyData, ReagentPropertyData
+from api.models import Keyword, MaterialSafetyData, ReagentPropertyData
 
+
+class KeywordSerializer(DjongoModelSerializer):
+    class Meta:
+        model = Keyword
+        fields = '__all__'
 
 class ReagentPropertyDataSerializer(DjongoModelSerializer):
     class Meta:
--- back/api/views.py
+++ back/api/views.py
@@ -3,30 +3,53 @@ from rest_framework.views import APIView
 from rest_framework.request import Request
 from rest_framework.response import Response
 
-from api.models import MaterialSafetyData, ReagentPropertyData
-from api.serializers import MaterialSafetyDataSerializer, ReagentPropertyDataSerializer
+from api.models import Keyword, MaterialSafetyData, ReagentPropertyData
+from api.serializers import KeywordSerializer, MaterialSafetyDataSerializer, ReagentPropertyDataSerializer
 from datetime import datetime
 
 
 class Search(APIView):
     def get(self, request, format=None):
         keywords = request.GET.get('keyword')
-        response = {
-            "OK" : [],
-            "NotFound" : [],
-            "Result" : []
-        }
+        response = []
         
         for keyword in keywords.split(','):
-            queryset = MaterialSafetyData.objects.filter(casNo=keyword)
-            serializer = MaterialSafetyDataSerializer(queryset, many=True)
-            if serializer.data:
-                response['OK'].append(keyword)
-                response['Result'].append(serializer.data[0])
-                print("[ /api/search/keyword=" + keyword + " ]", datetime.now(), "HTTP_200_OK")
+            newData = {} 
+            newData['Keyword'] = keyword
+            newCasID = getCasID(keyword)
+            if newCasID:
+                newData['ReagentData'] = getReagentPropertyData(newCasID)
+                newData['MaterialSafetyData'] = getMarterialSafetyData(newCasID)
+                print("[ /api/search?keyword={} ] {} 200_OK".format(keyword, datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
             else:
-                response['NotFound'].append(keyword)
-                print("[ /api/search/keyword=" + keyword + " ]", datetime.now(), "HTTP_404_NotFound")
-            
+                newData['ReagentData'] = None
+                newData['MaterialSafetyData'] = None
+                print("[ /api/search?keyword={} ] {} 404_NotFound".format(keyword, datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+            response.append(newData)
+
         return Response(response, status=status.HTTP_200_OK)
 
+
+def getCasID(keyword):
+    queryset = Keyword.objects.filter(keyword=keyword)
+    serializer = KeywordSerializer(queryset, many=True)
+    if serializer.data:
+        return serializer.data[0]['casNo']
+    else:
+        return None
+
+def getReagentPropertyData(casNo):
+    queryset = ReagentPropertyData.objects.filter(casNo=casNo)
+    serializer = ReagentPropertyDataSerializer(queryset, many=True)
+    if serializer.data:
+        return serializer.data[0]
+    else:
+        return None
+
+def getMarterialSafetyData(casNo):
+    queryset = MaterialSafetyData.objects.filter(casNo=casNo)
+    serializer = MaterialSafetyDataSerializer(queryset, many=True)
+    if serializer.data:
+        return serializer.data[0]
+    else:
+        return None
