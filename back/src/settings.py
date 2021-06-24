1c01c974a35ebba3ede085b6491724f5e61bf038
4Moyede <krkdgud9@gmail.com>
2021-06-25 02:27:52 +0900

    [B_Ref] fix git conflict

--- back/src/settings.py
+++ back/src/settings.py
@@ -16,6 +16,7 @@ import djongo
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+ROOT_DIR = os.path.dirname(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -26,7 +27,9 @@ SECRET_KEY = '6^#(fznj(p$byy)a8f&vh$dvy8_zz&-da@v2+lq&@bwuzz4+xn'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+]
 
 
 # Application definition
@@ -38,9 +41,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'api.apps.ApiConfig',
+    'accounts.apps.AccountsConfig',
+
     'rest_framework',
     'corsheaders',
-    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
@@ -74,10 +80,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'src.wsgi.application'
 
+# Key file path
+
+DATABASE_SETTINGS_FILE = os.path.join(os.path.join(ROOT_DIR, '.key'), 'db_settings.json')
+GOOGLE_API_SETTINGS_FILE = os.path.join(os.path.join(ROOT_DIR, '.key'), 'googleapi.json')
 
 # Database
 
-DATABASE_SETTINGS_FILE = os.path.join(os.path.join(ROOT_DIR, '.key'), 'db_settings.json')
 db_info = json.loads(open(DATABASE_SETTINGS_FILE).read())
 db_database = db_info['altas']['database']
 db_host = db_info['altas']['host']
@@ -154,3 +163,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Google API Setting
+
+GOOGLE_AUTH_SCOPES = [
+    'openid',
+    'https://www.googleapis.com/auth/userinfo.email',
+    'https://www.googleapis.com/auth/userinfo.profile',
+]
